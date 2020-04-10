d8ebe112a2da0689dc4bad344c6dbe2a01f8c6d3
nodm <nodm.pe@gmail.com>
2020-04-10 18:52:19 +0300

    + add NGRX router store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
+import { NavigationActionTiming, routerReducer, StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { environment } from '~env/environment';
-import { userFactory } from '~core/providers'
+import { userFactory } from '~core/providers';
 import { LayoutModule } from '~modules/layout';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,14 +21,21 @@ import { AppComponent } from './app.component';
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    StoreModule.forRoot({}, {
-      runtimeChecks: {
-        strictStateImmutability: true,
-        strictActionImmutability: true,
-        strictActionWithinNgZone: true,
-        strictStateSerializability: true,
-        strictActionSerializability: true,
+    StoreModule.forRoot(
+      {
+        router: routerReducer,
       },
+      {
+        runtimeChecks: {
+          strictStateImmutability: true,
+          strictActionImmutability: true,
+          strictActionWithinNgZone: true,
+          strictStateSerializability: true,
+          strictActionSerializability: true,
+        },
+    }),
+    StoreRouterConnectingModule.forRoot({
+      navigationActionTiming: NavigationActionTiming.PostActivation,
     }),
     StoreDevtoolsModule.instrument({
       maxAge: 25,
